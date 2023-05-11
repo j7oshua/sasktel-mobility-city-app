ac4655ea1651fae8f2de7e5139a302ca4bf11582
Joshua Grimard <jgrimard22@gmail.com>
2023-05-11 09:03:04 -0600

    Removed un-used import

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,16 @@
+import {Link} from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
 const Error = () => {
     return (
-        <div>
-            Error Page
-        </div>
+        <Wrapper className='full-page'>
+            <div>
+                <img src={img} alt='404error' />
+                <h3>Ohh! page not found</h3>
+                <p>We can't seem to find the page you're looking for</p>
+                <Link to='/'>back home</Link>
+            </div>
+        </Wrapper>
     )
 }
 
