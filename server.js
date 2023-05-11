0dbd3a0f770ac33bc38660220fa7cb0633db44c3
Joshua Grimard <jgrimard22@gmail.com>
2023-05-11 11:35:37 -0600

    Created a Register/Login page. Will be used by city employees to register with the app or login.

--- server.js
+++ server.js
@@ -1,37 +1,39 @@
-import "express-async-errors"
-import express from "express"
-const app = express()
-import dotenv from "dotenv"
-dotenv.config()
+import "express-async-errors";
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
 // DB
 import connectDB from "./db/connect.js";
 
 // Routers
-import billRouter from "./routes/monthlyBillRoutes.js"
+import authRouter from "./routes/authRoutes.js";
+import billRouter from "./routes/monthlyBillRoutes.js";
 
 // middleware
-import notFoundMiddleware from "./middleware/not-found.js"
-import errorHandlerMiddleware from "./middleware/error-handler.js"
+import notFoundMiddleware from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
 
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.use("/api/v1/monthly_bills", billRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/monthly_bills", billRouter);
 
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const start = async () => {
     try {
-        await connectDB()
+        await connectDB();
         app.listen(port, ()=> {
-            console.log(`Server is listening on port ${port}...`)
+            console.log(`Server is listening on port ${port}...`);
         })
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-start()
+start();
